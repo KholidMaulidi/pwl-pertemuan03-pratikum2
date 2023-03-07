f684591480e061742dea987efece460219203de9
anhar <fndedek@gmailcom>
2023-03-07 14:03:17 +0700

    chances

--- resources/views/Kholid/profileKholid.blade.php
+++ resources/views/Kholid/profileKholid.blade.php
@@ -1,14 +1,33 @@
 @extends('master')
+
 @section('parent')
 @parent
 @endsection
-@section
-<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Kholid Maulidi</h5>
-      <p class="card-text">Skuy Living</p>
-      <a href="#" class="btn btn-primary">view more</a>
-    </div>
-  </div>
+@section('kholidprofile')
+<h3 class="profile-username text-center">Kholid Maulidi</h3>
+<div class="text-center">
+  <img class="profile-user-img img-fluid img-circle"
+      src="../../dist/img/kholid-crop.jpeg"
+      alt="User profile picture">
+</div>
+
+                <p class="text-muted text-center">Mahasiswa</p>
+
+                <ul class="list-group list-group-unbordered mb-3">
+                  <li class="list-group-item">
+                    <b>TTL</b> <a class="float-right">Sumenep, 08 Mei 2003</a>
+                  </li>
+                  <li class="list-group-item">
+                    <b>Alamat</b> <a class="float-right">Sumenep</a>
+                  </li>
+                  <li class="list-group-item">
+                    <b>Email</b> <a class="float-right">kholidmaulidi85@gmail.com</a>
+                  </li>
+                </ul>
+
+                <a href="#" class="btn btn-primary btn-block"><b>View More</b></a>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
 @endsection
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -25,7 +25,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
+            <a href="{{Route('selamat')}}" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Dashboard
@@ -52,42 +52,7 @@
           </li>
         </ul>
       </nav>
-      <!-- /.sidebar-menu -->
     </div>
-
-    <!-- Sidebar Menu -->
-    <nav class="mt-3">
-      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
-        <li class="nav-item">
-          <a href="../widgets.html" class="nav-link">
-            <i class="nav-icon fas fa-th"></i>
-            <p>
-              Dashboard
-              <span class="right badge badge-danger">New</span>
-            </p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-copy"></i>
-            <p>
-              Data Mahasiswa
-              <span class="badge badge-info right">2</span>
-            </p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{Route('content_kuliah')}}" class="nav-link">
-            <i class="nav-icon far fa-image"></i>
-            <p>
-              Pengalaman Kuliah
-            </p>
-          </a>
-        </li>
-      </ul>
-    </nav>
     <!-- /.sidebar-menu -->
   </div>
   <!-- /.sidebar -->
