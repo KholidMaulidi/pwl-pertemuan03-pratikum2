01ce546b41c34f605448e1c6063bb62d45c0cb1c
aldinpramudya <aldinarielpramudya@gmail.com>
2023-03-07 15:32:58 +0700

    alert script

--- resources/views/layout/selamat.blade.php
+++ resources/views/layout/selamat.blade.php
@@ -5,6 +5,9 @@
 @endsection
 
 @section('selamat')
+<script>
+    alert("Selamat Datang");
+</script>
 <div class="card card-primary card-outline">
     <a class="d-block w-100" data-toggle="collapse" href="#collapseTwo">
         <div class="card-header">
